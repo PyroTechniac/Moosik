9e646f1064ac14cb769b0b44ef574dd5ac0ac4f4
Owen Bellish <shadowguy224@live.com>
2019-02-27 14:29:35 -0500

    work

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const ExtendedClient = require('./lib/Client');
 
-
-const cli = new ExtendedClient({ disableEveryone: true });
-cli.registerCommands().registerStructures();
-console.log(cli);
+new ExtendedClient({ disableEveryone: true }).login('Test');
